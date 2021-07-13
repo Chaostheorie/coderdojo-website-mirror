a500dab871fe306a442deae18d65b5878f021fc7
Cobalt <chaosthe0rie@pm.me>
2021-07-13 17:01:45 +0200

    update purgecss integration

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,26 @@ module.exports = {
         require('postcss-font-magician')(),
         require('postcss-viewport-height-correction'),
         require('postcss-pxtorem')(),
+        require('@fullhuman/postcss-purgecss')({
+            content: ['public/**/*.html', 'public/*.html'],
+            css: ['public/main.css'],
+            safelist: [
+                'toggled',
+                'sidebar-toggled',
+                'show',
+                'collapse',
+                'collapsing',
+                'bi-clipboard-check',
+            ],
+            fontFace: true,
+            keyframes: true,
+        }),
+        require('postcss-purgefonts')({
+            to: 'public/fonts/',
+            purge_only_fonts: ['bootstrap-icons'],
+            preserve_all_on_zero_matching_glyphs: false,
+            ignore_urls: false,
+        }),
         require('cssnano')({
             preset: 'advanced',
         }),
